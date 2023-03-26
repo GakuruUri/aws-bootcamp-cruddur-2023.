200743648a26ca6b3403aebcbe539fdc1d546c2a
Uri Gakuru Karanja <urigakuru@icloud.com>
2023-03-26 09:06:48 +0000

    Create user in cognito

--- aws/lambdas/cruddur-post-confirmation.py
+++ aws/lambdas/cruddur-post-confirmation.py
@@ -32,7 +32,7 @@ def lambda_handler(event, context):
         user_handle,
         user_cognito_id
       ]
-      cur.execute(sql,*params)
+      cur.execute(sql, params)
       conn.commit() 
 
     except (Exception, psycopg2.DatabaseError) as error:
@@ -43,3 +43,56 @@ def lambda_handler(event, context):
           conn.close()
           print('Database connection closed.')
     return event
+
+
+#DEBUGGING________________
+# import json
+# import psycopg2
+# import os
+
+# def lambda_handler(event, context):
+#     user = event['request']['userAttributes']
+#     print('userAttributes')
+#     print(user)
+
+#     user_display_name  = user['name']
+#     user_email         = user['email']
+#     user_handle        = user['preferred_username']
+#     user_cognito_id    = user['sub']
+#     try:
+#       print('entered-try')
+#       sql = f"""
+#          INSERT INTO public.users (
+#           display_name, 
+#           email,
+#           handle, 
+#           cognito_user_id
+#           ) 
+#         VALUES(
+#           {user_display_name},
+#           {user_email},
+#           {user_handle},
+#           {user_cognito_id}
+#         )
+#       """
+#       print('SQL Statement ----')
+#       print(sql)
+#       conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
+#       cur = conn.cursor()
+#       params = [
+#         user_display_name,
+#         user_email,
+#         user_handle,
+#         user_cognito_id
+#       ]
+#       cur.execute(sql, params)
+#       conn.commit() 
+
+#     except (Exception, psycopg2.DatabaseError) as error:
+#       print(error)
+#     finally:
+#       if conn is not None:
+#           cur.close()
+#           conn.close()
+#           print('Database connection closed.')
+#     return event
